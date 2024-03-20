c65fc5b4f543cb58a8332e89a6a7cf3d4a176c2c
mandy <107859104+eug2n2@users.noreply.github.com>
2024-03-20 16:00:31 +0900

    [Silver II] Title: 유기농 배추, Time: 180 ms, Memory: 16736 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -0,0 +1,63 @@
+import java.util.*;
+import java.awt.Point;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        Queue<Point> queue = new LinkedList<Point>(); // 배추좌표 들어갈 큐 
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); 
+        int t=Integer.parseInt(bf.readLine());
+        for (int s=1; s<=t; s++){
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+            int n= Integer.parseInt(st.nextToken()); //가로
+            int m = Integer.parseInt(st.nextToken()); //세로
+            int k = Integer.parseInt(st.nextToken()); //배추길이
+            
+            int[][] graph = new int [n][m];
+            int[][] visited = new int[n][m];
+            int keep =0; //인접해있는 배추가있는지 여부 
+            int ans =0;
+    
+            for (int i =0 ;i<k;i++){ // 배추입력받기
+                    st = new StringTokenizer(bf.readLine());
+                    int x= Integer.parseInt(st.nextToken()); 
+                    int y= Integer.parseInt(st.nextToken()); 
+                    graph[x][y]=1;
+                    queue.add(new Point(x,y));
+                }
+        
+            
+            while (!queue.isEmpty()) {
+                Point p= queue.poll();
+                if (graph[p.x][p.y]==1 &&visited[p.x][p.y]==0){
+                  
+                    ans+=1;
+                    visited[p.x][p.y]=1;
+                    visited=baechuu(graph,visited,queue,p.x,p.y,n,m);        
+                    }  
+                }
+                System.out.println(ans);
+             }   
+    }
+
+     public static int[][] baechuu( int[][] graph, int[][] visited,  Queue<Point> queue, int i, int j, int n, int m) {
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+        
+        for (int k = 0; k < 4; k++) {
+            int nx =i+ dx[k];
+            int ny = j + dy[k];
+    
+            // 범위 확인
+            if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
+                // 현재 위치가 1이고 방문하지 않은 경우
+                if (graph[nx][ny] == 1 && visited[nx][ny] == 0) { //인접배추가 있고, 방문안한 배추일경우 
+                    visited[nx][ny]=1;
+                    visited=baechuu(graph,visited,queue,nx,ny,n,m);
+                }
+            }
+        }
+        return visited; // 인접한 위치 중에 조건을 만족하는 것이 없으면 false 반환
+    }
+ }
