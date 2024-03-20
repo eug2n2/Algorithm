e9784bf1758097318514f54c986c5dfa248bd915
mandy <107859104+eug2n2@users.noreply.github.com>
2024-03-20 16:04:22 +0900

    [Gold IV] Title: 부분합, Time: 216 ms, Memory: 23232 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -0,0 +1,40 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(bf.readLine());
+		int[] map = new int[n+1];
+		for (int i = 0; i < n; i++) {
+			map[i] = Integer.parseInt(st.nextToken());
+		}
+		int start = 0;
+		int end = 0;
+		int min = Integer.MAX_VALUE;
+		int total = 0;
+		while (start <= n && end <= n) {
+			if (total >= c && min > end -start) {
+				min = end - start;
+			}
+			if (total < c) {
+				total += map[end++];
+			} else {
+				total -= map[start++];
+			}
+		}
+		if (min == Integer.MAX_VALUE) {
+			System.out.println(0);
+		} else {
+			System.out.println(min);
+		}
+
+	}
+
+}
