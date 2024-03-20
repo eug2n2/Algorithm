7daa245cd26e2430f6de6e46b2c471cae83c93e3
mandy <107859104+eug2n2@users.noreply.github.com>
2024-03-20 16:17:17 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 528 ms, Memory: 173908 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+class Main {
+	static int[] lis;
+	public static void main(String args[]) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(bf.readLine()); // 크기
+		int[] arr = new int[n];
+		// 길이
+		int[] dp = new int[n];
+		// 각 숫자의 수열
+
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		lis = new int[n+1];  // 길이가 len인 부분수열들의 마지막 값 중 최솟값
+		lis[0] = -1000000001;//가장 작은 숫자가 -10억이니까 
+		int idx=0;
+		int len = 0; // 가장 긴 길이 저장 
+		for (int i = 0; i < n; i++) {
+			if(arr[i] > lis[len]) {
+				dp[i] = ++len;
+				lis[len] =arr[i];
+			}else {
+				idx = binarySearch(0, len, arr[i]); // lis 배열 중 최솟값을 기준으로 위치를 탐색
+				lis[idx] = arr[i];
+				dp[i] = idx;
+			}
+
+		}
+	
+		System.out.println(len);
+
+	}
+	  
+		static int binarySearch(int left, int right, int key) {
+			int mid =0;
+			while(left < right) {
+				mid = (left+right)/2;
+				
+				if(lis[mid] < key) {
+					left = mid+1;
+				}else {
+					right = mid;
+				}
+			}
+			return right;
+		}
+}
