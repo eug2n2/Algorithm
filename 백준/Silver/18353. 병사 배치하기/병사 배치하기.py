0437668e8b4e8aa7a20a815db761a405512718df
mandy <107859104+eug2n2@users.noreply.github.com>
2024-03-20 16:21:58 +0900

    [Silver II] Title: 병사 배치하기, Time: 156 ms, Memory: 114488 KB -BaekjoonHub

---
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
@@ -0,0 +1,11 @@
+n=int(input())
+a=list(map(int,input().split()))
+dp=[1]*(n)
+for i in range(1,n):
+    for k in range(i):
+        if a[k]>a[i]:
+            dp[i]=max(dp[i],dp[k]+1)
+
+        
+print(n-max(dp))
+
