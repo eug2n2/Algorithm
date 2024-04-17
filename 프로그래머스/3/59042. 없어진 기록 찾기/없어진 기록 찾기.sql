cc2a1bc8af2aaf4f532c317955947cd35123d812
mandy <107859104+eug2n2@users.noreply.github.com>
2024-04-17 23:22:07 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -0,0 +1,5 @@
+# -- 코드를 입력하세요
+SELECT o.ANIMAL_ID ,o.Name
+from  ANIMAL_INS as i right outer join ANIMAL_OUTS as o on i.ANIMAL_ID= o.ANIMAL_ID
+where i.ANIMAL_ID is null
+order by o.ANIMAL_ID ,o.Name;
