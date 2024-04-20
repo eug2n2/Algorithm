241b6f199b53750eabfab2563f72a8ca89c6b6c0
mandy <107859104+eug2n2@users.noreply.github.com>
2024-04-20 13:06:43 +0900

    [level 5] Title: 멸종위기의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
@@ -0,0 +1,13 @@
+with recursive gen_data as(
+    select id, PARENT_ID, 1 as GENERATION
+    from ECOLI_DATA 
+    where PARENT_ID is null
+    union all
+    select e.id, e.parent_id, (g.GENERATION+1) as GENERATION
+    from ECOLI_DATA as e
+    inner join gen_data as g on e.parent_id= g.id
+)
+
+# select * from ECOLI_DATA as e right outer join gen_data as g on e.PARENT_ID =g.id 
+select (select count(*) from ECOLI_DATA as e right outer join gen_data as g on e.PARENT_ID =g.id where gg.GENERATION= g.GENERATION and e.id is null )as COUNT,GENERATION
+from gen_data as gg group by GENERATION order by GENERATION
