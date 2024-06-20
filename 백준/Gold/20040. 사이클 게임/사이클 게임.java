726b97f4ddf6bfd1dd7cf0b3669efd20b1694899
mandy <107859104+eug2n2@users.noreply.github.com>
2024-06-20 17:33:36 +0900

    [Gold IV] Title: 사이클 게임, Time: 592 ms, Memory: 133928 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[] parent ;
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		parent = new int[n]; //0 부터 n − 1 까지 고유한 번호가 부여된 평면 상의 점 n 개가 주어짐
+		for(int i=0;i<n;i++) {
+			parent[i]=i;
+		}
+		for(int i=0;i<m;i++) {
+			st = new StringTokenizer(bf.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+
+			if(!union(x,y)) {
+				System.out.println((i+1));
+				return;
+			}
+		}
+
+		System.out.println(0);
+		
+		
+	}
+	static int find(int a) {
+		if(parent[a]==a) return a;
+		return find(parent[a]);
+	}
+	static boolean union(int a, int b) {
+		int pa = find(a);
+		int pb = find(b);
+		if(pa==pb) return false;
+		if(pa<pb) parent[pb]=pa;
+		else parent[pa]=pb;
+		return true;
+	}
+}
