30fca3988483f6476009167424bb4eda749bfa41
yujini <107859104+eug2n2@users.noreply.github.com>
2024-08-29 09:22:16 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.50 ms, Memory: 82.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] jobs) {
+        int size = jobs.length;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
+        int time= 0;;
+        int idx =0;;
+        while (idx < size && jobs[idx][0] <= time) {
+                pq.offer(jobs[idx++]);
+        }
+            
+        int sum =0;
+        int cnt =0;   
+        while(cnt<size){
+      
+            // System.out.println(sum);   
+            while (idx < size && jobs[idx][0] <= time) {
+                pq.offer(jobs[idx++]);
+            }
+            
+            if (pq.isEmpty())  {
+				time = jobs[idx][0];
+            
+            }else{
+                int[] p =pq.poll();
+                time+=p[1];
+                cnt++;
+                sum +=(time-p[0]);
+            }
+            
+		}
+        
+        return sum/size;
+    }
+}
