a1fa56801b862ceab3f428524e9d80872a92933a
yujini <107859104+eug2n2@users.noreply.github.com>
2024-12-24 12:07:48 +0900

    [Gold II] Title: 대표 선수, Time: 956 ms, Memory: 153388 KB -BaekjoonHub

--- 백준/Gold/2461. 대표 선수/대표 선수.java
+++ 백준/Gold/2461. 대표 선수/대표 선수.java
@@ -8,43 +8,37 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         int answer = Integer.MAX_VALUE;
-
+        int max = 0;
         int[][] arr = new int[n][m];
-
-        int[] index = new int[n];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a ,b) -> a[0] - b[0]);
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(bf.readLine());
             for (int j = 0; j < m; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
-        }
-
-        for (int i = 0; i < n; i++) {
             Arrays.sort(arr[i]);
+            max =Math.max(max,arr[i][0]);
+            pq.offer(new int[]{arr[i][0], i, 0});
         }
+
         while(true){
-            int mintmp = Integer.MAX_VALUE;
-            int minidx =0;
-            int maxtmp = 0;
-            for (int i = 0; i < n; i++) {
+            int[] q = pq.poll();
+            int min = q[0];
+            int ntmp = q[1];
+            int mtmp =q[2];
 
-                int tmp = arr[i][index[i]];
-                if(tmp<mintmp){
-                    mintmp=tmp;
-                    minidx =i;
-                }
-                if(tmp>maxtmp){
-                    maxtmp=tmp;
-                }
-
-            }
-            int atmp =maxtmp-mintmp;
+            int atmp =max-min;
             if(answer>atmp) {
                 answer = atmp;
             }
-            if(++index[minidx]==m){
+            if(mtmp+1==m){
                 break;
+            }
+            int next = arr[ntmp][mtmp+1];
+            pq.add(new int[] {next, ntmp, mtmp+1});
+            if(next>max){
+                max = next;
             }
         }
         System.out.println(answer);
