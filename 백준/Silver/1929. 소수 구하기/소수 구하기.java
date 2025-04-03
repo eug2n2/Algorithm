d22377478a4dbdeba761f3f2382a67b05aae09a2
yujini <107859104+eug2n2@users.noreply.github.com>
2025-04-03 23:49:42 +0900

    [Silver III] Title: 소수 구하기, Time: 156 ms, Memory: 18096 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -4,31 +4,34 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-	BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-	StringTokenizer st =new StringTokenizer(bf.readLine());
-	StringBuilder sb = new StringBuilder();
-	int m =Integer.parseInt(st.nextToken());
-	int n=Integer.parseInt(st.nextToken());	
-	for (int i=m; i<=n;i++) {
-		if(sosu(i)) {
-			sb.append(i+"\n");
-		}
-	}
-	System.out.println(sb);	
-		}
-	
-	public static boolean sosu(int x) {
-		if( x==1) {
-			return false;
-		}
-		else{for (int i =2; i<=Math.sqrt(x);i++) {
-			if( x%i==0) {
-				return false;
-				}
-			}
-		}
-		return true;
-	}
-	
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+
+        boolean[] isPrime = new boolean[n + 1]; 
+        for (int i = 2; i <= n; i++) {
+            isPrime[i] = true; // 초기값 true 설정 (0과 1 제외)
+        }
+
+        for (int i = 2; i * i <= n; i++) {
+            if(!isPrime[i]) continue;
+            
+            for (int j = i * i; j <= n; j += i) { 
+                isPrime[j] = false; // i의 배수는 소수가 아님
+            }
+            
+        }
+
+        for (int i = m; i <= n; i++) {
+            if (isPrime[i]) {
+                sb.append(i).append("\n");
+            }
+        }
+
+        System.out.print(sb);
+    }
 }
