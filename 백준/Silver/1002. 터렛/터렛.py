07725b00c5ecbc4f990860b539f5b71ef54fe50f
yujini <107859104+eug2n2@users.noreply.github.com>
2025-08-07 17:44:07 +0900

    [Silver III] Title: 터렛, Time: 80 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/1002. 터렛/터렛.py
+++ 백준/Silver/1002. 터렛/터렛.py
@@ -1,28 +1,16 @@
-T=int(input()) # tc 수
+import sys
+T=int(sys.stdin.readline()) # tc 수
 for i in range(T):
-    x1,y1,r1,x2,y2,r2=map(int,input().split())
-    if (x1,y1) == (x2,y2) and r1 !=0 and r2 !=0:
-        if r1 == r2:
-            print(-1) 
-            continue
-        else:
-            print(0)
-            continue
-    elif (x1,y1) == (x2,y2) and r1 == 0 and r2 == 0:
-        print(1)
-        continue
-    elif (x1,y1) == (x2,y2):
-        print(0)
-        
-## 원이 만나는가??? 
+    x1,y1,r1,x2,y2,r2=map(int,sys.stdin.readline().split())
+
+    ## 원이 만나는가??? 
     d=((x1-x2)**2+(y1-y2)**2)**0.5 # 거리
-    if d > r1 + r2: # 두 원이 멀리 떨어져 있음
+
+    if d==0 and r1==r2: # 동심원 (같은 원의 중심, 같은 반지름)
+        print(-1)
+    elif d > r1 + r2 or d < abs(r1 - r2): # 두 원이 만나지 않거나, 한 원이 다른 원 안에 있음
         print(0)
-    elif d < abs(r1 - r2): # 한 원이 다른 원 안에 있음
-        print(0)
-    elif d == r1 + r2: # 외접
-        print(1)
-    elif d == abs(r1 - r2): # 내접
+    elif d == r1 + r2 or d == abs(r1 - r2): # 외접/ 내접
         print(1)
     else: # 두 원이 교차함
         print(2)
