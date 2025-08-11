fa21a224c91708229cf90e9728635ee5e8de2067
yujini <107859104+eug2n2@users.noreply.github.com>
2025-08-11 11:59:26 +0900

    [Bronze II] Title: 거스름돈, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/5585. 거스름돈/거스름돈.py
@@ -0,0 +1,17 @@
+import sys
+
+n = int(sys.stdin.readline())
+n=1000-n
+ans = 0
+ans += n// 500
+n= n % 500
+ans += n // 100
+n = n % 100
+ans += n // 50
+n = n % 50
+ans += n // 10
+n = n % 10
+ans += n // 5
+n = n % 5
+ans += n // 1
+print(ans)
