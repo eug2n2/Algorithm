b78bc6837e559274f5a15c738bf07237ebf84184
yujini <107859104+eug2n2@users.noreply.github.com>
2025-08-20 16:12:14 +0900

    [Silver IV] Title: 흑백 요리사, Time: 140 ms, Memory: 136100 KB -BaekjoonHub

---
+++ 백준/Silver/32653. 흑백 요리사/흑백 요리사.py
@@ -0,0 +1,12 @@
+import sys
+import math
+n = int(sys.stdin.readline())
+array = list(map(int, sys.stdin.readline().split()))
+# 최소공배수
+
+lcm= array[0]
+for i in range(1,n):
+    lcm = math.lcm(lcm, array[i])
+    
+lcm*=2
+print(lcm)
