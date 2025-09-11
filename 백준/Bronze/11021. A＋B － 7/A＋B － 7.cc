122f3783f879ef69de13f19a3e221f9f8e83d507
yujini <107859104+eug2n2@users.noreply.github.com>
2025-09-12 01:09:09 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, a, b;
+    cin >> num;  
+
+    for (int i = 0; i < num; i++) {
+        cin >> a >> b;  
+        cout << "Case #" <<i+1 <<": "<< a+b << "\n";
+    }
+
+    return 0;
+}
